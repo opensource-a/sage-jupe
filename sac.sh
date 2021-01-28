4e5487a094ea76de24a05d2996b7462d21b08c5d
opensource-a <62256831+opensource-a@users.noreply.github.com>
2021-01-29 03:39:50 +0530

    Update sac.sh

--- sac.sh
+++ sac.sh
@@ -1,20 +1,22 @@
 s3BucketName=sage-jupe-test-1001
 stackName=sage-jupe-1001
 permissionBoundary=arn:aws:iam::aws:policy/AdministratorAccess
-notebookName=powers.ipynb
+notebookName=convertor.ipynb
 
 aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary
 aws cloudformation wait stack-create-complete --stack-name $stackName
 
-tar xvf container.tar
+
 cd container
-chmod +x build-and-push.sh
-./build_and_push.sh notebook-runner
+chmod +x build_and_push.sh
+./build_and_push.sh notebook-runner-$stackName
 
 cd ..
 
 aws s3 mb s3://$s3BucketName
 aws s3 cp $notebookName s3://$s3BucketName/
 
-aws lambda invoke --function-name RunNotebook --payload "{\"input_path\": \"s3://$s3BucketName/$notebookName\", \"parameters\": {\"p\": 0.75}}" result.json
+aws s3 cp cities.csv s3://$s3BucketName/
+
+aws lambda invoke --function-name RunNotebook --payload "{\"input_path\": \"s3://$s3BucketName/$notebookName\", \"parameters\": {\"input_bucket\": \"$s3BucketName\"}}" result.json
 
