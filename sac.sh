c1a4506dcf9b126e1bda8e2c4d6c47feb5a1e3d7
opensource-a <62256831+opensource-a@users.noreply.github.com>
2021-01-30 00:12:44 +0530

    Update sac.sh

--- sac.sh
+++ sac.sh
@@ -1,11 +1,13 @@
-s3BucketName=sage-jupe-test-101
 stackName=sage-jupe-101
 permissionBoundary=arn:aws:iam::aws:policy/AdministratorAccess
 notebookName=convertor.ipynb
 
+awsAccountId=$(aws sts get-caller-identity --query Account --output text)
+s3InputBucket=$stackName-input-bucket-$awsAccountId
+s3OutputBucket=$stackName-output-bucket-$awsAccountId
+
 aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary
 aws cloudformation wait stack-create-complete --stack-name $stackName
-
 
 cd container
 chmod +x build_and_push.sh
@@ -13,10 +15,9 @@ chmod +x build_and_push.sh
 
 cd ..
 
-aws s3 mb s3://$s3BucketName
-aws s3 cp $notebookName s3://$s3BucketName/
+aws s3 cp $notebookName s3://$s3InputBucket/
 
-aws s3 cp cities.csv s3://$s3BucketName/
+aws s3 cp cities.csv s3://$s3InputBucket/
 
-aws lambda invoke --function-name RunNotebook --payload "{\"image\": \"notebook-runner-$stackName\", \"input_path\": \"s3://$s3BucketName/$notebookName\", \"parameters\": {\"input_bucket\": \"$s3BucketName\"}}" result.json
+aws lambda invoke --function-name RunNotebook --payload "{\"image\": \"notebook-runner-$stackName\", \"input_path\": \"s3://$s3InputBucket/$notebookName\", \"parameters\": {\"input_bucket\": \"$s3InputBucket\", \"output_bucket\": \"$s3OutputBucket\"}}" result.json
 
