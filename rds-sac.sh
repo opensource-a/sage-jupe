0db13370bc12b0d2148de49d1f6a5723b8731aba
opensource-a <62256831+opensource-a@users.noreply.github.com>
2021-02-24 00:37:52 +0530

    Update rds-sac.sh

--- rds-sac.sh
+++ rds-sac.sh
@@ -1,20 +1,13 @@
-stackName=sage-jupe-101
-permissionBoundary=arn:aws:iam::aws:policy/AdministratorAccess
-notebookName=rdsconvertor.ipynb
-securityGroup=<sg-id>
-subnetId=<subnet-id>
-rdsEndpoint=<rds-host-endpoint>
-rdsUser=admin
-rdsPassword=<password>
-database=cities
-query="select * from cities_data"
-output_prefix=output
+while read assign; do
+ export "$assign";
+done < <(sed -nE 's/([a-z_]+): (.*)/\1=\2/ p' parameters.yml)
+
 
 awsAccountId=$(aws sts get-caller-identity --query Account --output text)
-s3Bucket=$stackName-bucket-$awsAccountId
 
 
-aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary
+
+aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary ParameterKey=S3BucketName,ParameterValue=$s3bucket
 aws cloudformation wait stack-create-complete --stack-name $stackName
 
 cd container
@@ -23,20 +16,9 @@ chmod +x build_and_push.sh
 
 cd ..
 
-cp $notebookName injected-$notebookName
+aws s3 cp $notebookName s3://$s3bucket/notebooks/
 
-sed -i  "s/#input_bucket/$s3Bucket/g" injected-$notebookName
-sed -i  "s/#output_bucket/$s3Bucket/g" injected-$notebookName
-sed -i  "s/#output_prefix/$output_prefix/g" injected-$notebookName
-sed -i  "s/#rdsendpoint/$rdsEndpoint/g" injected-$notebookName
-sed -i  "s/#user/$rdsUser/g" injected-$notebookName
-sed -i  "s/#password/$rdsPassword/g" injected-$notebookName
-sed -i  "s/#querydatabase/$database/g" injected-$notebookName
-sed -i  "s/#querystring/$query/g" injected-$notebookName
+aws s3 cp cities.csv s3://$s3bucket/input/
 
-aws s3 cp injected-$notebookName s3://$s3Bucket/notebooks/
-
-aws s3 cp cities.csv s3://$s3Bucket/input/
-
-aws lambda invoke --function-name $stackName-RunNotebook --payload "{\"image\": \"notebook-runner-$stackName\", \"input_path\": \"s3://$s3Bucket/notebooks/injected-$notebookName\", \"extra_args\": {\"NetworkConfig\": {\"VpcConfig\": {\"SecurityGroupIds\": [\"$securityGroup\"], \"Subnets\": [\"$subnetId\"]}}}}" result.json
+aws lambda invoke --function-name $stackName-RunNotebook --payload "{\"image\": \"notebook-runner-$stackName\", \"input_path\": \"s3://$s3bucket/notebooks/$notebookName\", \"extra_args\": {\"NetworkConfig\": {\"VpcConfig\": {\"SecurityGroupIds\": [\"$securityGroup\"], \"Subnets\": [\"$subnetId\"]}}}}" result.json
 
