8175523dee1c33efc696338ada416c0a9ba67b25
opensource-a <62256831+opensource-a@users.noreply.github.com>
2021-02-25 00:19:15 +0530

    Update rds-sac.sh

--- rds-sac.sh
+++ rds-sac.sh
@@ -6,23 +6,24 @@ done < <(sed -nE 's/([a-z_]+): (.*)/\1=\2/ p' parameters.yml)
 
 
 awsAccountId=$(aws sts get-caller-identity --query Account --output text)
+stackName=runnotebook-$notebookName-$(date "+%Y%m%d-%H%M%S")
 
 
 
 #aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary ParameterKey=S3BucketName,ParameterValue=$s3bucket
-aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation2.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary ParameterKey=S3BucketName,ParameterValue=$s3bucket ParameterKey=S3NotebookPrefix,ParameterValue=$notebook_prefix ParameterKey=S3NotebookKey,ParameterValue=$notebookName ParameterKey=ProcessingInstanceType,ParameterValue=$processingInstanceType ParameterKey=ProcessingJobSecurityGroup,ParameterValue=$securityGroup ParameterKey=ProcessingJobSubnetId,ParameterValue=$subnetId  
+aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary ParameterKey=S3InputBucketName,ParameterValue=$inputs3bucket ParameterKey=S3InputPrefix1,ParameterValue=$input_prefix1 ParameterKey=S3InputPrefix2,ParameterValue=$input_prefix2 ParameterKey=S3TempBucketName,ParameterValue=$temp_bucket ParameterKey=S3NotebookPrefix,ParameterValue=$stackName/notebooks ParameterKey=S3NotebookKey,ParameterValue=$notebookName.ipynb ParameterKey=ProcessingInstanceType,ParameterValue=$processingInstanceType ParameterKey=ProcessingJobSecurityGroup,ParameterValue=$securityGroup ParameterKey=ProcessingJobSubnetId,ParameterValue=$subnetId ParameterKey=Timestamp,ParameterValue=$(date "+%Y%m%d-%H%M%S")  
 aws cloudformation wait stack-create-complete --stack-name $stackName
 
-cp parameters.yml container/parameters.yml
+cp $notebookPath/$notebookName_parameters.yml container/parameters.yml
 cd container
 chmod +x build_and_push.sh
-./build_and_push.sh notebook-runner-$stackName
+./build_and_push.sh $stackName
 
 cd ..
 
-aws s3 cp $notebookName s3://$s3bucket/$notebook_prefix/
+aws s3 cp $notebookPath/$notebookName.ipynb s3://$temp_bucket/$stackName/notebooks/$notebookName.ipynb
 
-aws s3 cp cities.csv s3://$s3bucket/$input_prefix/
+#aws s3 cp cities.csv s3://$inputs3bucket/$input_prefix1/
 
 #aws lambda invoke --function-name $stackName-RunNotebook --payload "{\"image\": \"notebook-runner-$stackName\", \"input_path\": \"s3://$s3bucket/notebooks/$notebookName\", \"extra_args\": {\"NetworkConfig\": {\"VpcConfig\": {\"SecurityGroupIds\": [\"$securityGroup\"], \"Subnets\": [\"$subnetId\"]}}}}" result.json
 
