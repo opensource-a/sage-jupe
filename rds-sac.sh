f953c36c8a752f3c6b5e154c387154db0f9eceff
opensource-a <62256831+opensource-a@users.noreply.github.com>
2021-02-25 03:37:59 +0530

    Update rds-sac.sh

--- rds-sac.sh
+++ rds-sac.sh
@@ -17,6 +17,14 @@ echo $stackName
 aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary ParameterKey=S3InputBucketName,ParameterValue=$inputs3bucket ParameterKey=S3InputPrefix1,ParameterValue=$input_prefix1 ParameterKey=S3InputPrefix2,ParameterValue=$input_prefix2 ParameterKey=S3TempBucketName,ParameterValue=$temp_bucket ParameterKey=S3NotebookPrefix,ParameterValue=$stackName/notebooks ParameterKey=S3NotebookKey,ParameterValue=$notebookName.ipynb ParameterKey=ProcessingInstanceType,ParameterValue=$processingInstanceType ParameterKey=ProcessingJobSecurityGroup,ParameterValue=$securityGroup ParameterKey=ProcessingJobSubnetId,ParameterValue=$subnetId ParameterKey=Timestamp,ParameterValue=$timestamp  
 aws cloudformation wait stack-create-complete --stack-name $stackName
 
+lambdaarn=arn:aws:lambda:us-east-1:$awsAccountId:function:$stackName-invoke
+cp s3triggerlambdaconfigtemplate.json s3triggerlambdaconfig.json
+sed -i  "s/#lambdaarn/$lambdaarn/g" s3triggerlambdaconfig.json
+sed -i  "s/#prefix1/$input_prefix1/g" s3triggerlambdaconfig.json
+sed -i  "s/#prefix2/$input_prefix2/g" s3triggerlambdaconfig.json
+
+aws s3api put-bucket-notification-configuration --bucket $inputs3bucket --notification-configuration file://s3triggerlambdaconfig.json
+
 cp $notebookPath/${notebookName}_parameters.yml container/parameters.yml
 cd container
 chmod +x build_and_push.sh
