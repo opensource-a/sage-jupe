41fb4db3ef65753bdf49ac945123517b0218684b
opensource-a <62256831+opensource-a@users.noreply.github.com>
2021-02-25 02:17:01 +0530

    Update rds-sac.sh

--- rds-sac.sh
+++ rds-sac.sh
@@ -17,7 +17,7 @@ echo $stackName
 aws cloudformation create-stack --stack-name $stackName --template-body file://$(pwd)/cloudformation.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=PermissionsBoundary,ParameterValue=$permissionBoundary ParameterKey=S3InputBucketName,ParameterValue=$inputs3bucket ParameterKey=S3InputPrefix1,ParameterValue=$input_prefix1 ParameterKey=S3InputPrefix2,ParameterValue=$input_prefix2 ParameterKey=S3TempBucketName,ParameterValue=$temp_bucket ParameterKey=S3NotebookPrefix,ParameterValue=$stackName/notebooks ParameterKey=S3NotebookKey,ParameterValue=$notebookName.ipynb ParameterKey=ProcessingInstanceType,ParameterValue=$processingInstanceType ParameterKey=ProcessingJobSecurityGroup,ParameterValue=$securityGroup ParameterKey=ProcessingJobSubnetId,ParameterValue=$subnetId ParameterKey=Timestamp,ParameterValue=$timestamp  
 aws cloudformation wait stack-create-complete --stack-name $stackName
 
-cp $notebookPath/$notebookName_parameters.yml container/parameters.yml
+cp $notebookPath/${notebookName}_parameters.yml container/parameters.yml
 cd container
 chmod +x build_and_push.sh
 ./build_and_push.sh $stackName
